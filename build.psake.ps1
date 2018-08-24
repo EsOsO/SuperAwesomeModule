f997eba00ba9117a3b4a4b570d1fbd431e6f4636
Massimo Bonvicini <massimo.bonvicini@unipolsai.it>
2018-08-24 10:19:48 +0200

    fixxxxx

--- build.psake.ps1
+++ build.psake.ps1
@@ -1,8 +1,60 @@
-Properties {
+function Update-AdditionalReleaseArtifact {
+    param(
+        [string] $Version
+    )
+
+    Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value $Version
 }
+
+Properties {
+    $GitVersion = gitversion | ConvertFrom-Json
+    $Artifact = '{0}.zip' -f $GitVersion.SemVer
+    $ArtifactPath = Join-Path $env:BHBuildOutput $Artifact
+}
+
+FormatTaskName (('-' * 25) + ('[ {0,-28} ]') + ('-' * 25))
 
 Task Default -Depends Build
 
-Task Build {
+Task Init {
+    Write-Host ('Working folder: {0}' -f $PWD)
+    Write-Host ('GitVersion: {0}' -f $GitVersion.SemVer)
+    Write-Host ('Git Branch: {0}' -f $GitVersion.BranchName)
+}
 
+Task IncrementVersion -Depends Init {
+    trap {
+        Pop-Location
+        Write-Error "$_"
+        exit 1
+    }
+
+    Push-Location $env:BHProjectPath
+
+    $PendingChanges = git status --porcelain
+    if ($null -ne $PendingChanges) {
+        throw 'You have pending changes, aborting release'
+    }
+
+    Exec {git fetch origin}
+    Exec {git checkout master}
+    Exec {git merge origin/master --ff-only}
+
+    Update-AdditionalReleaseArtifact -Version $GitVersion.MajorMinorPatch
+
+    Exec {git commit -am "Create release ${GitVersion.MajorMinorPatch}" --allow-empty}
+    Exec {git tag ${GitVersion.MajorMinorPatch}}
+
+    if ($LASTEXITCODE -ne 0) {
+        Exec {git reset --hard HEAD^}
+        throw 'No changes detected since last release'
+    }
+
+    Exec {git push origin master --tags}
+
+    Pop-Location
+}
+
+Task Build -Depends IncrementVersion {
+    Compress-Archive -Path $env:BHModulePath -DestinationPath $ArtifactPath
 }
